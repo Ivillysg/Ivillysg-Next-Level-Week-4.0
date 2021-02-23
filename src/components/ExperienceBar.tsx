1ba5ea519c38313108bf4a423c3bd9a086e64578
ivillysg <ivillysgomes@gmail.com>
2021-02-23 09:55:51 -0300

    :sparkles: added components and created countdown hook

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import styles from '../styles/components/ExperienceBar.module.css'
 
-const ExperienceBar = () => {
+export const ExperienceBar = () => {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: '50%' }} />
-        <span  className="current-experience" style={{left:'50%'}}>
+        <span  className={styles.currentExperience} style={{left:'50%'}}>
           300xp
         </span>
       </div>
@@ -15,4 +16,3 @@ const ExperienceBar = () => {
   );
 };
 
-export default ExperienceBar;
